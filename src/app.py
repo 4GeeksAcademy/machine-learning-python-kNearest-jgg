fc49f7b91aa7afc4c90c8479bddc61d9e103f340
Javier González <javigon258@gmail.com>
2025-05-12 19:20:49 +0000

    Main

--- src/app.py
+++ src/app.py
@@ -30,3 +30,6 @@ def predict():
         resultado = f"Error en la predicción: {e}"
 
     return render_template('index.html', resultado=resultado)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
